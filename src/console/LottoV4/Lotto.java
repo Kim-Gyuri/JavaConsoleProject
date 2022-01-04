bae0059f458972fba945d5ce5611c1d20ffe49cc
Kim-Gyuri <gyuring0203@gmail.com>
2022-01-04 16:15:32 +0900

    (자바 콘솔) 좀 더 객체지향스럽게 만든 로또 게임 버전 4, --> 버전 3을 수정해서 만들 수는 있을까?

---
+++ src/console/LottoV4/Lotto.java
@@ -0,0 +1,31 @@
+package console.LottoV4;
+
+import java.util.ArrayList;
+
+public class Lotto {
+    private ArrayList<Integer> lotto; //6개의 번호를 담을 저장소
+
+    //기본 생성자
+    public Lotto(ArrayList<Integer> lotto) {
+        this.lotto = lotto;
+    }
+
+    //getter
+    public ArrayList<Integer> getLotto() {
+        return lotto;
+    }
+
+    //setter
+    public void setLotto(ArrayList<Integer> lotto) {
+        this.lotto = lotto;
+    }
+
+    @Override
+    public String toString() {
+        String info = "";
+        for (Integer a : lotto) {
+            info += " " + a;
+        }
+        return info;
+    }
+}
