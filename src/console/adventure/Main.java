c42a478cf62bc03cdd36251bb1d00f18f982c395
Kim-Gyuri <gyuring0203@gmail.com>
2022-01-13 00:00:21 +0900

    콘솔 어드벤처 게임 1

---
+++ src/console/adventure/Main.java
@@ -0,0 +1,16 @@
+package console.adventure;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+
+        //
+        String[] enemies = {..}
+        int max;
+        int enemyAttack
+    }
+}
